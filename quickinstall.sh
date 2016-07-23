17b93072d1b905adc1c7779f36113796aabacf98
Andrew Shumate <ashumate@users.noreply.github.com>
2016-07-22 23:07:36 -0400

    Update quickinstall.sh

--- quickinstall.sh
+++ quickinstall.sh
@@ -34,3 +34,12 @@ curl -L https://get.rvm.io | bash -s stable --ruby
 #PCAP Everything
 echo -e "\nRunning docker: pcap...\n"
 docker run -v ~/pcap:/pcap --net=host -d jgamblin/tcpdump
+
+#Add unprivileged user account
+echo -e "\nAdding unpriviliged user\n"
+useradd ashumate
+mkdir -p /home/ashumate/.ssh/authorized_keys
+cat ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDMcsKIY7QfLFSGMNwLdVzk2/5CYPTaR2nO8pqdr0uKNIZxrXCt3r84RfIcq2sb6IMyHa7oUUth3gqYnQFGdMvINubENkdgsCTLFjzAe8kpMtzDBvlaQtFfeGY6/X/O5JXjrbc90viMdzHDqmArAplIRIlPQGDFwKGqv+MWvPihCrn2IZLXIco78IF+pgDjCcOJur6dIS1MQvbicoaJhZpIB/Oa1zqsCYeDpc3jj7FusmL3ml+8OTg5WmgUJ1GWcX4xsYndIn6TIN+OcqsN/w5kD7f51QiQxOpgVwxF329amxOcreEcBHVPeexV59boEKD6dPLbuXwWe9YyK6ZGXEwUOCax1azz6uXgMPwuQkEe8en3rvkp6kWXVZJk9Xaxw0TNnx2OT/ibzlRMiaoNqhl7qAU/uVSJ+91Mcp3dN3kVuYUnIwYja33sP2SzFNeED0zcX0rHrvbuM/Q/u+QZQ1vPDuhBpURmXb0zfZA7Z1QSPRvuqbPuDEf3rnkY7wEDqty7/JFRAAbnkWobGIwADMjTHYLWL28OCdPjnZ7b6s3V0h6bOl2L1kn/qhkgLTvVvQW9utEOFlxSZZkaYtUijmdRh5m651mTDvGOB0QTQbRbrmfMtVTQuXdpH68kwtTyH412VsTiB1+JjQ9Af7/kiTdJg9vf1Kpyv/QEroJimPkt2w== ashumate@gmail.com > /home/ashumate/.ssh/authorized_keys
+
+#Reboot server
+reboot now
